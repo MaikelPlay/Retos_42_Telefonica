d60c6f7553473127cefb574cbe0ea398ae533828
Maikel <maikelcorreoelectronico@gmail.com>
2025-11-05 16:39:32 +0100

    Challenge 07: Configuración de GitHub Actions CI

--- Retos_CI-CD-main/challenge07/starter.c
+++ Retos_CI-CD-main/challenge07/starter.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int suma(int a, int b);
+int suma(int a, int b) {
+    return a + b;
+}
 
+#ifndef TESTING
 int main() {
+    int a = 5, b = 3;
     printf("CI con GitHub Actions listo.\n");
+    printf("Prueba de suma: %d + %d = %d\n", a, b, suma(a, b));
     return 0;
 }
+#endif
